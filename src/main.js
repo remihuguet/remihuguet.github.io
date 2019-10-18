569485af8ea9468413fd69feb6c2931366e17637
Rémi Huguet <revanphy@gmail.com>
2019-10-18 09:17:15 +0200

    Test fonts

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 // Import main css
 import '~/assets/style/index.scss'
-import 'typeface-merriweather';
+// import 'typeface-merriweather';
 import 'typeface-oswald';
+import 'typeface-cooper-hewitt';
+// import 'typeface-playfair-display';
+// import 'typeface-aileron';
+import 'typeface-abhaya-libre';
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
 
